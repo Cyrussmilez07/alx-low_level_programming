0754869addc13eef26eacf793139337a22afd429
Cyrussmilez07 <effiongcyrus390@gmail.com>
2021-11-25 09:29:58 -0800

    commit on lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,11 +1,7 @@
- 
-#ifndef LISTS_H
-#define LIST_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -17,12 +13,10 @@
  */
 typedef struct dlistint_s
 {
-  int n;
-  struct dlistint_s *prev;
-  struct dlistint_s *next;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 } dlistint_t;
-
-/* prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -33,4 +27,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* idndef LIST_H */
+#endif /* _LISTS_H_ */
